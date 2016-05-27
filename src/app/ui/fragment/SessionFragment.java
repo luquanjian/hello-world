0015b10a6d19aa80d218961fffe2052e3be8d5c0
luquanjian <820575257@qq.com>
2016-05-27 21:36:55 +0800

    first commiting

---
+++ src/app/ui/fragment/SessionFragment.java
@@ -0,0 +1,25 @@
+package app.ui.fragment;
+
+import mobi.kuaidian.qunakao.R;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import app.ui.BaseFragment;
+
+public class SessionFragment extends BaseFragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_session, container, false);
+    }
+    /* (non-Javadoc)
+     * @see app.ui.BaseFragment#onViewCreated(android.view.View, android.os.Bundle)
+     */
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
